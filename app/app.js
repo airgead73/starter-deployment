6c0163051de8a2dc1b25a9718a64b74c5209ee1d
Dan Moneypenny <catullus1973@gmail.com>
2022-11-18 06:12:02 -0800

    update

--- app/app.js
+++ app/app.js
@@ -37,7 +37,7 @@ app.use(function(err, req, res, next) {
     status = err.status;
   }
 
-  res.send(`STATUS: ${status}, ERROR: ${err.message}`);
+  res.send(`PATH: ${req.path}, ERROR: ${err.message}`);
 
 });
 /**
