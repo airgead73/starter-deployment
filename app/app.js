fd710960d7f4be14719459488efd01518b5459b7
Dan Moneypenny <catullus1973@gmail.com>
2022-11-18 05:33:43 -0800

    add basic index and app

--- app/app.js
+++ app/app.js
@@ -1,1 +1,30 @@
-// app file for application
+/**
+ * external imports
+ */
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+/**
+ * app activation
+ */
+const app = express();
+/**
+ * middleware
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+/**
+ * routes
+ */
+app.get((req, res, next) => {
+  res.send('Practice for deploying apps.');
+});
+/**
+ * error handling
+ */
+/**
+ * export app
+ */
+module.exports = app;
